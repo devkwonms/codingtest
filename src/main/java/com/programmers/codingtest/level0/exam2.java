300d4eba32ba4f726d95f2ba4d2a4a2593d4a4f2
devkwonms <alstj1379@nate.com>
2023-06-08 03:01:45 +0900

    a와b 출력하기

---
+++ src/main/java/com/programmers/codingtest/level0/exam2.java
@@ -0,0 +1,16 @@
+package com.programmers.codingtest.level0;
+
+import java.util.Scanner;
+
+public class exam2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+
+        sc.close();
+    }
+}
