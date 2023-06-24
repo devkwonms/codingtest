f0979dce4dee4da52624b944bfbb39a19a127699
devkwonms <alstj1379@nate.com>
2023-06-24 20:38:22 +0900

    공배수 문제 설명 정수 number와 n, m이 주어집니다. number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록 solution 함수를 완성해주세요.
    제한사항
    10 ≤ number ≤ 100
    2 ≤ n, m < 10
    입출력 예
    number	n	m	result
    60	2	3	1
    55	10	5	0
    입출력 예 설명
    입출력 예 #1
    
    60은 2의 배수이면서 3의 배수이기 때문에 1을 return합니다.
    입출력 예 #2
    
    55는 5의 배수이지만 10의 배수가 아니기 때문에 0을 return합니다.

--- src/main/java/com/programmers/codingtest/level0/exam17.java
+++ src/main/java/com/programmers/codingtest/level0/exam17.java
@@ -9,14 +9,6 @@ public class exam17 {
         solution(number,n,m);
     }
     public static int solution(int number, int n, int m) {
-        int answer = 0;
-
-        if((number % n == 0) && (number % m == 0)){
-            answer = 1;
-        }else{
-            answer = 0;
-        }
-        System.out.println("answer = " + answer);
-        return answer;
+        return number % n == 0 && number % m == 0 ? 1 : 0;
     }
 }
